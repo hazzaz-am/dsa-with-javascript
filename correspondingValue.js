bdb7bd19b24b75036245c139bc7353f22181db5e
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-01-02 23:27:36 +0600

    corresponding value problem solved

---
+++ correspondingValue.js
@@ -0,0 +1,26 @@
+// check if first arrays number is present in second array with power of 2
+
+/**
+ *
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @returns {Boolean}
+ */
+
+function correspondingValue(arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	for (let i = 0; i < arr1.length; i++) {
+		const currentIndex = arr2.indexOf(arr1[i] ** 2);
+
+		if (currentIndex === -1) {
+			return false;
+		}
+
+		arr2.splice(currentIndex, 1);
+	}
+	return true;
+}
+console.log(correspondingValue([2, 3, 4], [16, 9, 4]));
