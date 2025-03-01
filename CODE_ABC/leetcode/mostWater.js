5883803996b9eaac88bb42e9f757f97a76f0d9e8
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-03-01 16:15:44 +0600

    solve leetcode problems

---
+++ CODE_ABC/leetcode/mostWater.js
@@ -0,0 +1,41 @@
+// var maxArea = function (height) {
+// 	let right = height.length - 1;
+// 	let left = 0;
+// 	let maxContainer = 0;
+// 	for (let i = left; i < height.length; i++) {
+// 		for (let j = right; j > i; j--) {
+// 			if (height[i] < height[j]) {
+// 				let minHeight = Math.min(height[left], height[right]);
+// 				let area = (right - left) * minHeight;
+// 				maxContainer = Math.max(maxContainer, area);
+// 				left++;
+// 				break;
+// 			} else {
+// 				let minHeight = Math.min(height[left], height[right]);
+// 				let area = (right - left) * minHeight;
+// 				maxContainer = Math.max(maxContainer, area);
+//         right--;
+// 			}
+// 		}
+// 	}
+
+// 	return maxContainer;
+// };
+
+var maxArea = function (height) {
+	let right = height.length - 1;
+	let left = 0;
+	let maxContainer = 0;
+
+	while (left < right) {
+		let minHeight = Math.min(height[left], height[right]);
+		let area = (right - left) * minHeight;
+		maxContainer = Math.max(maxContainer, area);
+
+		height[left] < height[right] ? left++ : right--;
+	}
+
+	return maxContainer;
+};
+
+console.log(maxArea([1, 1]));
