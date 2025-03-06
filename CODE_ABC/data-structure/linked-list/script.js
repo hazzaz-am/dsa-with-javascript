dc3d1c19434d89764a3c4efe9489287a9288aa3c
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-03-06 10:36:47 +0600

    practice linkedlist

--- CODE_ABC/data-structure/linked-list/script.js
+++ CODE_ABC/data-structure/linked-list/script.js
@@ -73,10 +73,16 @@ class SinglyLinkedList {
 	}
 
 	unShift(value) {
-		let currentNode = this.head;
 		const newNode = new Node(value);
-		newNode.next = currentNode;
-		this.head = newNode;
+		const currentNode = this.head;
+
+		if (this.isEmpty()) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head = newNode;
+			this.head.next = currentNode;
+		}
 		this.length++;
 	}
 
@@ -91,13 +97,111 @@ class SinglyLinkedList {
 	}
 }
 
-const list = new SinglyLinkedList();
-list.push(10);
-list.push(20);
-list.push(40);
-// list.pop();
-// list.shift();
-list.unShift(30);
+class Node {
+	constructor(value) {
+		this.next = null;
+		this.value = value;
+	}
+}
+
+class SinglyLinkedList2 {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	isEmpty() {
+		return this.length === 0;
+	}
+
+	push(value) {
+		const newNode = new Node(value);
+
+		if (this.isEmpty()) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+	}
+
+	unShift(value) {
+		const newNode = new Node(value);
+		const currentNode = this.head;
+
+		if (this.isEmpty()) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head = newNode;
+			this.head.next = currentNode;
+		}
+		this.length++;
+	}
+
+	pop() {
+		if (!this.head) {
+			return null;
+		}
+
+		if (this.length === 1) {
+			let newLastNode = this.head.next;
+			this.head = null;
+			this.tail = null;
+			this.length = 0;
+			return newLastNode;
+		}
+
+		let currentNode = this.head;
+		let newLastNode;
+
+		while (currentNode) {
+			if (currentNode.next === this.tail) {
+				newLastNode = currentNode;
+				break;
+			}
+			currentNode = currentNode.next;
+		}
+
+		newLastNode.next = null;
+		this.tail = newLastNode;
+		this.length--;
+	}
+
+	shift() {
+		if (this.length === 1) {
+			this.tail = null;
+		}
+
+		let currentNode = this.head;
+		this.head = currentNode.next;
+		this.length--;
+	}
+
+	showList() {
+		let arr = [];
+		let currentNode = this.head;
+
+		while (currentNode) {
+			arr.push(currentNode.value);
+			currentNode = currentNode.next;
+		}
+
+		return arr;
+	}
+}
+
+const list = new SinglyLinkedList2();
+list.unShift(40);
 list.unShift(50);
+list.push(30);
+list.push(20);
+list.push(10);
+list.pop();
+list.shift();
 console.log(list.showList());
 console.log(list);
+console.log(list.isEmpty());
