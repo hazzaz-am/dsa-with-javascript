c2321b22b6bdca0aae1766339e4d4ff527e71d97
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-03-24 10:15:01 +0600

    solve rotate array with one by one rotation solution

--- greeks/array/basic-problems/practice.js
+++ greeks/array/basic-problems/practice.js
@@ -1,59 +1,14 @@
-// reverse an array
+// rotate an array
 
-function reverseArray(arr) {
-	let i = 0;
-	let j = arr.length - 1;
-
-	while (j > i) {
-		let temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-		i++;
-		j--;
+function rotateOneByOne(arr, times) {
+	for (let i = 0; i < times; i++) {
+		let lastElem = arr[arr.length - 1];
+		for (let j = 0; j < arr.length; j++) {
+			arr[arr.length - 1 - j] = arr[arr.length - 1 - (j + 1)];
+		}
+		arr[0] = lastElem;
 	}
-
 	return arr;
 }
 
-function tempArray(arr) {
-	const len = arr.length;
-	let newArr = new Array(len);
-
-	for (let i = 0; i < arr.length; i++) {
-		newArr[i] = arr[len - i - 1];
-	}
-
-	for (let i = 0; i < arr.length; i++) {
-		arr[i] = newArr[i];
-	}
-
-	return arr;
-}
-
-function swappingElements(arr) {
-	const len = arr.length;
-	for (let i = 0; i < len / 2; i++) {
-		let currLast = arr[len - i - 1];
-		let temp = arr[i];
-		arr[i] = currLast;
-		currLast = temp;
-	}
-
-	return arr;
-}
-
-function recursiveFN(arr, left, right) {
-	if (left >= right) {
-		return;
-	}
-
-	[arr[left], arr[right]] = [arr[right], arr[left]];
-	recursiveFN(arr, left + 1, right - 1);
-}
-
-function recursiveCall(arr) {
-	recursiveFN(arr, 0, arr.length - 1);
-	return arr;
-}
-
-console.log(recursiveCall([1, 2, 3, 4, 5, 6]));
+console.log(rotateOneByOne([1, 2, 3, 4, 5, 6], 2));
